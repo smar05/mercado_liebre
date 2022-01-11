ef0f1cbe67a5f65e5eeede0c80a423a726020619
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-01-10 21:10:35 -0500

    Pagina de login

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/register", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
 app.listen(puerto, () =>
   console.log("Servidor corriendo en el puerto: " + puerto)
 );
